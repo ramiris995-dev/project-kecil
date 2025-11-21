d9243c0e0cef83a1f5a1e78419342a9437e4b36a
ramiristhegreat <ramiris995@gmail.com>
2025-11-22 05:33:36 +0700

    Create main.py

---
+++ stopwatch/main.py
@@ -0,0 +1,9 @@
+import time
+
+input("Tekan Enter untuk mulai...")
+start = time.time()
+
+input("Tekan Enter untuk berhenti...")
+end = time.time()
+
+print("Waktu:", round(end - start, 2), "detik")
